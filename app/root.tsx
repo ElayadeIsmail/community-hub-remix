e678f52f5f33bff9942964d24a7dbd25e18864aa
Elayade Ismail <elayadeismail@gmail.com>
2024-05-03 22:04:21 +0100

    Add tailwin fonts and shadcn

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,6 @@
+import type { LinksFunction } from "@remix-run/node";
+import { cssBundleHref } from "@remix-run/css-bundle";
+
 import {
   Links,
   Meta,
@@ -5,6 +8,15 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import tailwindStylesheet from "~/styles/tailwind.css?url";
+import fontStylesheet from "~/styles/font.css?url";
+
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: tailwindStylesheet },
+  { rel: "stylesheet", href: fontStylesheet },
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+]
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
