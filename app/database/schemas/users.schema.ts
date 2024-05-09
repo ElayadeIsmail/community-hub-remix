da8e9b56dd5dba261c3aabc7e3ce90de818193e9
Elayade Ismail <elayadeismail@gmail.com>
2024-05-09 23:59:54 +0100

    add db client and schema

---
+++ app/database/schemas/users.schema.ts
@@ -0,0 +1,50 @@
+import { createId } from '@paralleldrive/cuid2';
+import { relations, sql } from 'drizzle-orm';
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+
+export const users = sqliteTable('users', {
+	id: text('id')
+		.primaryKey()
+		.$defaultFn(() => createId()),
+	username: text('username').unique().notNull(),
+	email: text('email').unique().notNull(),
+	createdAt: integer('created_at', { mode: 'timestamp' })
+		.notNull()
+		.default(sql`CURRENT_TIMESTAMP`),
+	updatedAt: integer('updated_at', { mode: 'timestamp' })
+		.default(sql`CURRENT_TIMESTAMP`)
+		.$onUpdateFn(() => new Date()),
+});
+
+export const usersRelations = relations(users, ({ one, many }) => ({
+	password: one(passwords),
+	sessions: many(sessions),
+}));
+
+export const passwords = sqliteTable('passwords', {
+	hash: text('hash').notNull(),
+	userId: text('user_id').references(() => users.id),
+	createdAt: integer('created_at', { mode: 'timestamp' })
+		.notNull()
+		.default(sql`CURRENT_TIMESTAMP`),
+	updatedAt: integer('updated_at', { mode: 'timestamp' })
+		.default(sql`CURRENT_TIMESTAMP`)
+		.$onUpdateFn(() => new Date()),
+});
+
+export const sessions = sqliteTable('sessions', {
+	id: text('id')
+		.primaryKey()
+		.$defaultFn(() => createId()),
+	agent: text('agent').notNull(),
+	expiresAt: integer('expires_at', { mode: 'timestamp_ms' }).notNull(),
+	userId: text('user_id')
+		.notNull()
+		.references(() => users.id),
+	createdAt: integer('created_at', { mode: 'timestamp' })
+		.notNull()
+		.default(sql`CURRENT_TIMESTAMP`),
+	updatedAt: integer('updated_at', { mode: 'timestamp' })
+		.default(sql`CURRENT_TIMESTAMP`)
+		.$onUpdateFn(() => new Date()),
+});
