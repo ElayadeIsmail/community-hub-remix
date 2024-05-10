05600a90721617546ed9efea7ebc80b9578102c2
Elayade Ismail <elayadeismail@gmail.com>
2024-05-10 20:13:24 +0100

    add utils functions

--- app/utils/misc.tsx.ts
+++ app/utils/misc.tsx.ts
@@ -1,6 +1,38 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+/**
+ * Combine multiple header objects into one (uses append so headers are not overridden)
+ */
+export function combineHeaders(
+	...headers: Array<ResponseInit['headers'] | null>
+) {
+	const combined = new Headers();
+	for (const header of headers) {
+		if (!header) continue;
+		for (const [key, value] of new Headers(header).entries()) {
+			combined.append(key, value);
+		}
+	}
+	return combined;
+}
+
+/**
+ * Combine multiple response init objects into one (uses combineHeaders)
+ */
+export function combineResponseInits(
+	...responseInits: Array<ResponseInit | undefined>
+) {
+	let combined: ResponseInit = {};
+	for (const responseInit of responseInits) {
+		combined = {
+			...responseInit,
+			headers: combineHeaders(combined.headers, responseInit?.headers),
+		};
+	}
+	return combined;
 }
