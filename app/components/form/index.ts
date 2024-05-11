a235d81bc3bf61070263952f3a8530d7f2a4b17f
Elayade Ismail <elayadeismail@gmail.com>
2024-05-12 00:37:49 +0100

    refactor forms

--- app/components/form/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './errors-list';
-export * from './input-field';
--- app/components/ui/index.ts
+++ app/components/ui/index.ts
@@ -1,4 +1,6 @@
 export * from './button';
 export * from './checkbox';
 export * from './input';
+export * from './input-otp';
 export * from './label';
+export * from './textarea';
